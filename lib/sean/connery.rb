83c40a2c040a89776c2bd687580591d9a1817a01
Jonah Roth <jroth@covermymeds.com>
2016-10-28 14:52:23 -0400

    It'sh a shtart

--- lib/sean/connery.rb
+++ lib/sean/connery.rb
@@ -2,17 +2,27 @@ require "sean/connery/version"
 
 module Sean
   module Connery
+    S_REGEX = /([^_]?)([s][sh]*|z)([\s$]?)/
+    C_REGEX = /([^_]?)c([eiy])/
+    ESS_REGEX = /_s([\s_]|$)/
+
     def self.included(base)
       base.extend(ClassMethods)
       # TODO - smarten up the replacement
       base.methods.find_all { |m| m.to_s.include?('s') }.each do |method|
         owner = base.method(method).owner
-        owner.send :alias_method, method.to_s.gsub('s','sh').to_sym, method
+        owner.send :alias_method, replace_s_sounds(method.to_s).to_sym, method
       end
     end
 
     module ClassMethods
       # TODO - .new?
+    end
+
+    private
+
+    def replace_s_sounds(str)
+      str.gsub(S_REGEX, '\1sh\3').gsub(C_REGEX, '\1sh\2').gsub('x', 'ksh').gsub(ESS_REGEX, '_esh\1')
     end
   end
 end
